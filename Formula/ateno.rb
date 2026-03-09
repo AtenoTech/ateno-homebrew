f949942b0c9dbf96ff7a46c3736103d6b1a06a42
Ateno Robot <robot@ateno.co>
2026-03-09 06:44:33 +0000

    Update ateno to v0.1.4

--- Formula/ateno.rb
+++ Formula/ateno.rb
@@ -4,8 +4,8 @@ class Ateno < Formula
   desc "Ateno CLI"
   homepage "https://github.com/AtenoTech/ateno-py"
   # Bump version to v0.1.3 so the "Robot" triggers a fresh hash calculation
-  url "https://github.com/AtenoTech/ateno-py/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "c4b4c62fb3539c404d980fb77d7b72aeadeaddd9804fcae0ba92e6483cf46a0f" # Robot will update this
+  url "https://github.com/AtenoTech/ateno-py/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "7284bd80d2f2ca3838e1a7dbf4176733695ef11cc1b0dde05186adc091f17d7b" # Robot will update this
   license "MIT"
 
   depends_on "python@3.12"
