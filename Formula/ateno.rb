7d559a8dee077b14783ee45358d2efc7cc895c75
sviatoslavgladyshev <sg7622@nyu.edu>
2026-03-09 14:08:36 +0800

    fix: change local paths to github urls

--- Formula/ateno.rb
+++ Formula/ateno.rb
@@ -1,7 +1,7 @@
 class Ateno < Formula
   desc "Ateno CLI"
   homepage "https://github.com/AtenoTech/ateno-py"
-  url "file:///Volumes/DevelopmentSSD/Ateno/sdk/ateno-homebrew/ateno-0.1.1.tar.gz"
+  url "https://github.com/AtenoTech/ateno-py/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "f13157415c504401855677b47def1ad6839d41f705e9b6c45b6d0a82115aa04b"
   license "MIT"
 
