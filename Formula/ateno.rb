0c54d41e5b6af95fbbac9271d6933949274ae3f9
Ateno Robot <robot@ateno.co>
2026-03-11 06:51:53 +0000

    Update ateno to v0.1.6

--- Formula/ateno.rb
+++ Formula/ateno.rb
@@ -1,8 +1,8 @@
 class Ateno < Formula
   desc "Ateno CLI"
   homepage "https://github.com/AtenoTech/ateno-py"
-  url "https://github.com/AtenoTech/ateno-py/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "d8def16d5f9cfe1a07b04fddbff10464b7bb4d49324108831f2c10b3925ba62b"
+  url "https://github.com/AtenoTech/ateno-py/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "e5b7c152c27b7218748ffab965d3436d58ad050647dcd342d4fd2bd7770063fd"
   license "MIT"
 
   depends_on "python@3.12"
