6d0c25407d70fe0f76e2da606ec5d5f26805f3ce
Ateno Robot <robot@ateno.co>
2026-03-11 06:50:24 +0000

    Update ateno-js to v0.1.5

--- Formula/ateno-js.rb
+++ Formula/ateno-js.rb
@@ -1,8 +1,8 @@
 class AtenoJs < Formula
   desc "Ateno JavaScript SDK"
   homepage "https://github.com/AtenoTech/ateno-js"
-  url "https://github.com/AtenoTech/ateno-js/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "ba71a10afc03a870482963733abe3db6c1fdfc0afc63a1f2814460d74f49d74e"
+  url "https://github.com/AtenoTech/ateno-js/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "446e35043a3e61cb368fe84acb37294b7969836fbf6406ad539324280a479b19"
   depends_on "node"
 
   def install
